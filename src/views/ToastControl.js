1c374732b5989b66817344f7384776f26f62377d
zzhangJ <55151626+johntheZ@users.noreply.github.com>
2024-03-21 11:26:14 -0400

    added rerender counter

--- src/views/ToastControl.js
+++ src/views/ToastControl.js
@@ -13,8 +13,9 @@ import {
 } from 'react-native';
 import Toast from 'react-native-toast-message';
 
-
+let rr = 0;
 export function ToastControl() {
+    rr++;
     const showToast = () => {
         Toast.show({
             type: 'success',
@@ -25,6 +26,7 @@ export function ToastControl() {
 
     return (
         <View>
+            <Text>{"Toast Control RR: " + rr}</Text>
             <Button
                 title='show toast'
                 onPress={showToast}
